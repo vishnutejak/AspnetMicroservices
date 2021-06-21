9ba937c275eca6e83585544a79774c286fc265e5
vishnutejak <vishnutejak@gmail.com>
2021-06-21 12:35:30 +0530

    Discount API again

--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -1,3 +1,4 @@
+using Discount.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -10,7 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Discount.API.Repositories;
 
 namespace Discount.API
 {
